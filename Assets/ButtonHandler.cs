eef5ee89a7cecaea833f5bf44601086317e2ffd1
Craana <aa2233@student.jamk.fi>
2021-12-15 09:54:08 +0200

    Added start menu

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+	public void GameScene()
+	{
+		SceneManager.LoadScene(1);
+	}	
+	
+	
+}
